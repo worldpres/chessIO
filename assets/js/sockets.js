29dcdbe9524e24ea29a2b2ec7c3fc89fd955782c
Dominik <thepressu@gmail.com>
2019-01-13 16:19:16 +0100

    correct for socketio because of browser-sync proxy

--- assets/js/sockets.js
+++ assets/js/sockets.js
@@ -1,6 +1,7 @@
 $(() => {
 
-    let socket = io();
+    // let socket = io();
+    let socket = io('http://localhost:3000', { transports: ['websocket']}); //correct for socketio because of browser-sync proxy
 
     socket.on('welcome', (name) => {
         if (localStorage.name) socket.emit('thats my name', localStorage.name);
